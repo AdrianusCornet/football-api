84fd03e2d9cd70fda624fca2d2059d9570004a1d
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 16:50:33 +0200

    difine json parser

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const teamRouter = require('./team/router')
 const port = process.env.PORT || 4000
 
 const app = express()
+const jsonParser = bodyParser.json()
 
 app.listen(port, () => console.log(`Listening on :${port}`))
 app.use(teamRouter)
