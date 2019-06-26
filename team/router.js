23c989fc5a437e7dd2699a9149c6d894e52d92ab
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 15:57:13 +0200

    return error ore resold

--- team/router.js
+++ team/router.js
@@ -2,3 +2,16 @@ const { Router } = require('express')
 const Team = require('./model')
 
 const router = new Router
+
+router.get('/team', (request, response) => {
+  Team.query('SELECT * FROM football_team', (error, result) => {
+    if(error) {
+      response.status(500).send({
+        message: "server error (it's not your fault it's mine)",
+        details: error.message
+      })
+    } else {
+      response.send(result)
+    }
+  })
+})
