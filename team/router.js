6322143efe97d191400ceeaf5ef8f7812d43ba28
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 16:05:01 +0200

    fix router

--- team/router.js
+++ team/router.js
@@ -3,15 +3,10 @@ const Team = require('./model')
 
 const router = new Router
 
-router.get('/team', (request, response) => {
-  Team.query('SELECT * FROM football_team', (error, result) => {
-    if(error) {
-      response.status(500).send({
-        message: "server error (it's not your fault it's mine)",
-        details: error.message
-      })
-    } else {
-      response.send(result)
-    }
-  })
+router.get('/team', (request, response, next) => {
+  Team.findAll
+    .then(response)
+    .catch(next)
 })
+
+module.exports = router
