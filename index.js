986889632252b0c7ee75abf7ae39432d2ba2b8b7
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 13:53:23 +0200

    undo changes in wrong file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const Sequelize = require('sequelize')
 
-const databaseUrl = 'postgres://postgres:fb-api@localhost:5432/postgres'
 const port = process.env.PORT || 4000
 
 const app = express()
