8937420db99e684ab92e977db5ad296479fa40fc
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 14:16:24 +0200

    sync and export database

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
 const Sequelize = require('sequelize')
+
+const databaseUrl = 'postgres://postgres:fb-api@localhost:5432/postgres'
+
+const sequelize = new Sequelize(databaseUrl)
+
+sequelize
+  .sync()
+  .than(() => console.log('Database schema updated'))
+  .catch(console.error)
+
+module.exports = sequelize
