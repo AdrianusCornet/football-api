cf2ef7e20722c8f0cdd42c234fcf6a9cfc5aa646
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 17:49:52 +0200

    return team

--- team/router.js
+++ team/router.js
@@ -10,6 +10,13 @@ router.get(
     .then(teams => response.send(teams))
     .catch(next)
 )
+router.get(
+  '/team/:id',
+  (request, response, next) => Team
+    .findByPk()
+    .then(team => response.send(team))
+    .catch(next)
+)
 router.post(
   '/team',
   (request, response, next) => Team
