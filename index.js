b332f1bc9a449447b6a8d4187fc9c435a3f6d40d
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 16:49:41 +0200

    import body parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const database = require('./db')
 const team = require('./team/model')
 const teamRouter = require('./team/router')
