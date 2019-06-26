462e384acf4ab7b5ac53b86c290e4217ff4d0b13
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-26 16:15:12 +0200

    respond corectly

--- team/router.js
+++ team/router.js
@@ -3,10 +3,12 @@ const Team = require('./model')
 
 const router = new Router
 
-router.get('/team', (request, response, next) => {
-  Team.findAll
-    .then(response)
+router.get(
+  '/team',
+  (request, response, next) => Team
+    .findAll()
+    .then(teams => response.send(teams))
     .catch(next)
-})
+)
 
 module.exports = router
