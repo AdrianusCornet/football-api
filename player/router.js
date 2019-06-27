7e5e32f50a39670157f7a2cf70ec11add9241611
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 12:26:27 +0200

    get players

--- player/router.js
+++ player/router.js
@@ -2,3 +2,13 @@ const { Router } = require('express')
 const Player = require('./model')
 
 const router = new Router
+
+router.get(
+  '/player',
+  (request, response, next) => Player
+    .findAll()
+    .then(players => response.send(players))
+    .catch(next)
+)
+
+module.exports = router
