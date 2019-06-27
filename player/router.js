f6ef3b4e30b527e66bfb3b4e88a2a0c460dec21c
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 13:56:43 +0200

    return spesifick player

--- player/router.js
+++ player/router.js
@@ -7,7 +7,14 @@ router.get(
   '/player',
   (request, response, next) => Player
     .findAll()
-    .then(players => response.send(players))
+    .then(players => response.send({ players }))
+    .catch(next)
+)
+router.get(
+  '/player/:id',
+  (request, response, next) => Player
+    .findByPk(request.params.id)
+    .then(player => response.send({ player }))
     .catch(next)
 )
 
