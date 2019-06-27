39e971960b1916d5e8c657581acb3612a6177433
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-27 11:45:30 +0200

    start player model

--- player/model.js
+++ player/model.js
@@ -1,2 +1,17 @@
 const Sequelize = require("sequelize")
 const db = require('../db')
+
+const Player = db.define(
+  'player',
+  {
+    name: {
+      type: Sequelize.STRING,
+      field: 'player_name'
+    },
+    number: {
+      type: Sequelize.INTEGER,
+      field: 'player_number'
+    }
+  },
+  { tableName: 'football_player' }
+)
